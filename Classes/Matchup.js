57650b71d3f1d70e4bb4f0bb79a03e24e0a44190
adhe223 <adhe223@g.uky.edu>
2016-10-29 15:22:42 -0600

    Small change

--- Classes/Matchup.js
+++ Classes/Matchup.js
@@ -2,7 +2,6 @@ function Matchup(opponentName, pointsFor, pointsAgainst, victory) {
     this.opponentName = opponentName;
     this.pointsFor = pointsFor;
     this.pointsAgainst = pointsAgainst;
-    this.victory = victory;
 }
 
 module.exports = Matchup;
